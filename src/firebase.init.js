70edcc2d02847889173c19450be9387e970cb06a
arjahid <jhjahidhasanar@gmail.com>
2025-02-13 12:14:49 +0600

    added navbar

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDO7R0zJoO7FfZFpxk-ExXZNy4tDd_3joc",
+  authDomain: "new-milon.firebaseapp.com",
+  projectId: "new-milon",
+  storageBucket: "new-milon.firebasestorage.app",
+  messagingSenderId: "1098081674347",
+  appId: "1:1098081674347:web:d1aa27a257128812967d2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
